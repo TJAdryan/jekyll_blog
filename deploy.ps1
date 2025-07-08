ebb6c3894c11437ab2a801a644c989d87f7273da
Azure DevOps CD Pipeline <azure-devops@.com>
2025-07-08 18:38:38 -0400

    update azure_pipeline

--- deploy.ps1
+++ deploy.ps1
@@ -11,8 +11,11 @@ git config --global user.name "Azure DevOps CD Pipeline"
 # Define the GitHub Pages branch
 $gitHubPagesBranch = "$(githubPagesBranch)"
 
-# Checkout the main branch
-git checkout main
+# Define the artifact directory
+$artifactDir = "$(Pipeline.Workspace)/$(artifactName)"
+
+# Checkout the gh-pages branch
+git checkout $gitHubPagesBranch
 
 # Remove all existing files (except .git folder) to ensure a clean deploy.
 Write-Host "Cleaning existing files in $(Get-Location)"
@@ -21,18 +24,12 @@ Get-ChildItem -Path . -Exclude ".git" -Recurse -Force | ForEach-Object {
 }
 
 # Copy the newly built site files from the artifact download directory
-Write-Host "Copying built site from $(Pipeline.Workspace)/$(artifactName) to $(Get-Location)"
-Copy-Item -Path "$(Pipeline.Workspace)/$(artifactName)/*" -Destination "." -Recurse -Force
+Write-Host "Copying built site from $artifactDir to $(Get-Location)"
+Copy-Item -Path "$artifactDir/*" -Destination "." -Recurse -Force
 
 # Add all changes, commit, and push to the GitHub Pages branch
 git add .
 git commit -m "Azure DevOps CD: Deployed new blog content - $(Build.BuildId) [skip ci]" -ErrorAction SilentlyContinue
-
-# Checkout the gh-pages branch
-git checkout $gitHubPagesBranch
-
-# Merge the changes from main into gh-pages
-git merge main
 
 Write-Host "Pushing changes to GitHub Pages branch $gitHubPagesBranch"
 git push origin $gitHubPagesBranch
