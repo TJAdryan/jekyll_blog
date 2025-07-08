5299a9710a05d0667d68fb26b335902695698e68
Azure DevOps CD Pipeline <azure-devops@.com>
2025-07-08 18:45:34 -0400

    update azure_pipeline2

--- deploy.ps1
+++ deploy.ps1
@@ -11,21 +11,9 @@ git config --global user.name "Azure DevOps CD Pipeline"
 # Define the GitHub Pages branch
 $gitHubPagesBranch = "$(githubPagesBranch)"
 
-# Define the artifact directory
-$artifactDir = "$(Pipeline.Workspace)/$(artifactName)"
-
-# Checkout the gh-pages branch
-git checkout $gitHubPagesBranch
-
-# Remove all existing files (except .git folder) to ensure a clean deploy.
-Write-Host "Cleaning existing files in $(Get-Location)"
-Get-ChildItem -Path . -Exclude ".git" -Recurse -Force | ForEach-Object {
-    Remove-Item -LiteralPath $_.FullName -Recurse -Force
-}
-
 # Copy the newly built site files from the artifact download directory
-Write-Host "Copying built site from $artifactDir to $(Get-Location)"
-Copy-Item -Path "$artifactDir/*" -Destination "." -Recurse -Force
+Write-Host "Copying built site from $(Pipeline.Workspace)/$(artifactName) to $(Get-Location)"
+Copy-Item -Path "$(Pipeline.Workspace)/$(artifactName)/*" -Destination "." -Recurse -Force
 
 # Add all changes, commit, and push to the GitHub Pages branch
 git add .
