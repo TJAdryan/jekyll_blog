1956cacadc6be5766e9eb7bd2303876c77b1a4df
Azure DevOps CD Pipeline <azure-devops@TJAdryan/jekyll_blog.com>
2025-07-19 17:18:46 +0000

    Azure DevOps CD: Deployed new blog content [skip ci]

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752945217',resources: [
+    cacheName: 'chirpy-1752945489',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
